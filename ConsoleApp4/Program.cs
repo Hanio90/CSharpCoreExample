637b4e909a426f998ced882d56dbda796d64e32d
Hanio90 <hanio90@gmail.com>
2018-01-23 13:49:59 -0500

    this is my first commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int y = int.Parse(Console.ReadLine());
+            int z = x;
+
+            if (y == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                for (int i = 1; i < y; i++)
+                {
+                    z *= x;
+                }
+                Console.WriteLine(z);
+            }
+        }    
+    }
+}
